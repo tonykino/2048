866d10e50c09ea601347d9f16adeaddffc16aea8
Nathan LAFARGE <nlafarge@e2r2p20.clusters.42paris.fr>
2022-03-19 13:49:03 +0100

    start_menu

--- srcs/menus/ft_start_menu.c
+++ srcs/menus/ft_start_menu.c
@@ -1,23 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_debug.c                                         :+:      :+:    :+:   */
+/*   ft_start_menu.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nlafarge <nlafarge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/19 01:00:19 by nlafarge          #+#    #+#             */
-/*   Updated: 2022/03/19 13:45:44 by nlafarge         ###   ########.fr       */
+/*   Created: 2022/03/19 13:06:09 by nlafarge          #+#    #+#             */
+/*   Updated: 2022/03/19 13:45:10 by nlafarge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../2048.h"
 
-/*
-	FOR DEBUG PURPOSES ONLY
-	Prints all the debug information on the screen
-*/
-
-void	ft_debug(t_vars *vars)
+void	ft_start_menu(t_vars *vars)
 {
-	printw("BONJOUR \n\n%d\n\n", vars->key);
+	while (1)
+	{
+		clear();
+		ft_debug(vars);
+		refresh();
+		vars->key = getch();
+	}
 }
