b92554fb6e7ececd404c79fa3e44dcae7b78a129
Nathan LAFARGE <nlafarge@e2r2p20.clusters.42paris.fr>
2022-03-19 19:42:26 +0100

    Update

--- srcs/menus/ft_size_menu.c
+++ srcs/menus/ft_size_menu.c
@@ -1,40 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_leaderboard.c                                   :+:      :+:    :+:   */
+/*   ft_size_menu.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nlafarge <nlafarge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/19 17:04:00 by nlafarge          #+#    #+#             */
-/*   Updated: 2022/03/19 19:21:24 by nlafarge         ###   ########.fr       */
+/*   Created: 2022/03/19 18:01:50 by nlafarge          #+#    #+#             */
+/*   Updated: 2022/03/19 19:33:23 by nlafarge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../2048.h"
 
-void	ft_leaderboard(t_vars *vars)
+void	ft_size_menu(t_vars *vars)
 {
 	int	i;
-	char *leaderboard[] = {
-		" JEAN ",
-		" LOUIS ",
-		" MICHEL ",
-		" CLAUDETTE ",
-		" HUGO ",
-		" LOUISE ",
-		" JEANNETTE ",
-		" JEAN-CLAUDE ",
-		" BERNARD ",
-		" LUC "
-	};
-	int	leaderboard_size = sizeof(leaderboard) / sizeof(char *);
-	int leaderboard_height = leaderboard_size; // int = nb lines hard coded in the print of the menu
-
 	char *menu[] = {
+		" 4by4 ",
+		" 5by5 ",
 		" BACK "
 	};
-	int menu_size = sizeof(menu) / sizeof(char *);
-	int menu_height = leaderboard_height + 5;
+	int	menu_size = sizeof(menu) / sizeof(char *);
+	int menu_height = 3 + menu_size; // int = nb lines hard coded in the print of the menu
 
 	vars->key = 0;
 	vars->selected_menu_pos = 0;
@@ -52,6 +39,10 @@ void	ft_leaderboard(t_vars *vars)
 			vars->selected_menu_pos = -1;
 			break;
 		}
+		else if (ft_is_up_key(vars))
+			vars->selected_menu_pos = ft_select_menu(-1, vars->selected_menu_pos, menu_size);
+		else if (ft_is_down_key(vars))
+			vars->selected_menu_pos = ft_select_menu(1, vars->selected_menu_pos, menu_size);
 		else if (ft_is_enter_key(vars))
 			break;
 
@@ -63,22 +54,25 @@ void	ft_leaderboard(t_vars *vars)
 		ft_vertical_align(vars, menu_height);
 		ft_print_center(vars, "2048");
 		ft_break_lines(vars, 2);
-		for (i = 0; i < leaderboard_size; i++)
-		{
-			ft_print_center(vars, leaderboard[i]);
-			ft_break_line(vars);
-		}
 		for (i = 0; i < menu_size; i++)
 		{
-			ft_break_line(vars);
-			attron(A_REVERSE);
+			if (i == 2)
+				ft_break_line(vars);
+			if (vars->selected_menu_pos == i)
+				attron(A_REVERSE);
 			ft_print_center(vars, menu[i]);
-			attroff(A_REVERSE);
+			if (vars->selected_menu_pos == i)
+				attroff(A_REVERSE);
+			ft_break_line(vars);
 		}
 		refresh();
 		vars->key = getch();
 	}
 
-	if (vars->selected_menu_pos == 0) // Back
+	if (vars->selected_menu_pos == 0) // 4 X 4
+		ft_launch_game(vars);
+	else if (vars->selected_menu_pos == 1) // 5 X 5
+		ft_launch_game(vars);
+	else if (vars->selected_menu_pos == 2) // Back
 		ft_start_menu(vars);
 }
--- srcs/menus/ft_start_menu.c
+++ srcs/menus/ft_start_menu.c
@@ -6,77 +6,82 @@
 /*   By: nlafarge <nlafarge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 13:06:09 by nlafarge          #+#    #+#             */
-/*   Updated: 2022/03/19 17:05:26 by nlafarge         ###   ########.fr       */
+/*   Updated: 2022/03/19 19:41:56 by nlafarge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../2048.h"
-
-void	ft_print_center(t_vars *vars, char *str)
-{
-	int	len;
-	int	x;
-
-	len = ft_strlen(str);
-	x = (vars->nb_char_cols - len) / 2;
-	mvprintw(vars->actual_cursor_line, x, "%s", str);
-}
 
 void	ft_start_menu(t_vars *vars)
 {
 	int	i;
 	char *menu[] = {
 		" PLAY ",
-		" LEADERBOARD ",
+		" TOP SCORES ",
 		" EXIT "
 	};
 	int	menu_size = sizeof(menu) / sizeof(char *);
-	int menu_height = 7 + menu_size; // 7 = nb lines hard coded in the print of the menu
-	int	selected_menu_pos = 0;
+	int menu_height = 3 + menu_size; // int = nb lines hard coded in the print of the menu
+
+	vars->key = 0;
+	vars->selected_menu_pos = 0;
+
+	int	menu_width = 13;
 	
 	while (1)
 	{
 		i = 0;
 		ft_clear(vars); // My own clear function to also clear other variables (like cursor position)
-
-		/*
-			MENU LOGIC
-		*/
-		if (ft_is_esc_key(vars))
-			break;
-		else if (ft_is_up_key(vars))
-			selected_menu_pos = ft_select_menu(-1, selected_menu_pos, menu_size);
-		else if (ft_is_down_key(vars))
-			selected_menu_pos = ft_select_menu(1, selected_menu_pos, menu_size);
-		else if (ft_is_enter_key(vars))
-		{
-			if (selected_menu_pos == 0) // Play
-				ft_launch_game(vars);
-			else if (selected_menu_pos == 1) // Leaderboard
-				ft_leaderboard(vars);
-			else if (selected_menu_pos == 2) // Exit
-				break;
-		}
-
-		/*
-			ACTUALLY PRINT THE MENU
-		*/
-		//ft_debug(vars);
 		ft_get_win_size(vars);
-		ft_vertical_align(vars, menu_height);
-		ft_break_lines(vars, 4);
-		ft_print_center(vars, "2048");
-		ft_break_lines(vars, 2);
-		for (i = 0; i < menu_size; i++)
+
+		if (vars->nb_char_cols < menu_width || vars->nb_char_lines < menu_height)
 		{
-			if (selected_menu_pos == i)
-				attron(A_REVERSE);
-			ft_print_center(vars, menu[i]);
-			if (selected_menu_pos == i)
-				attroff(A_REVERSE);
-			ft_break_line(vars);
+			ft_vertical_align(vars, 1);
+			ft_print_center(vars, "TOO SMALL");
 		}
+		else
+		{
+			/*
+				MENU LOGIC
+			*/
+			if (ft_is_esc_key(vars))
+			{
+				vars->selected_menu_pos = -1; // Otherwise it would be 0 and just relaunch the menu
+				break;
+			}
+			else if (ft_is_up_key(vars))
+				vars->selected_menu_pos = ft_select_menu(-1, vars->selected_menu_pos, menu_size);
+			else if (ft_is_down_key(vars))
+				vars->selected_menu_pos = ft_select_menu(1, vars->selected_menu_pos, menu_size);
+			else if (ft_is_enter_key(vars))
+				break;
+
+			/*
+				ACTUALLY PRINT THE MENU
+			*/
+			//ft_debug(vars);
+			ft_vertical_align(vars, menu_height);
+			ft_print_center(vars, "2048");
+			ft_break_lines(vars, 2);
+			for (i = 0; i < menu_size; i++)
+			{
+				if (i == 2)
+					ft_break_line(vars);
+				if (vars->selected_menu_pos == i)
+					attron(A_REVERSE);
+				ft_print_center(vars, menu[i]);
+				if (vars->selected_menu_pos == i)
+					attroff(A_REVERSE);
+				ft_break_line(vars);
+			}
+		}
+		
 		refresh();
 		vars->key = getch();
 	}
+
+	if (vars->selected_menu_pos == 0) // Play
+		ft_size_menu(vars);
+	else if (vars->selected_menu_pos == 1) // Leaderboard
+		ft_leaderboard(vars);
 }
