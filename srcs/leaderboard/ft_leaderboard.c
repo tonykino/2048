5ef41e62989a1f3429fd7bbb6d7e63c063457f67
Nathan Lafarge <webmastersandyou@gmail.com>
2022-03-20 02:18:50 +0100

    update

--- srcs/leaderboard/ft_leaderboard.c
+++ srcs/leaderboard/ft_leaderboard.c
@@ -3,50 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   ft_leaderboard.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tokino <tokino@student.42.fr>              +#+  +:+       +#+        */
+/*   By: nlafarge <nlafarge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 17:04:00 by nlafarge          #+#    #+#             */
-/*   Updated: 2022/03/20 00:46:44 by tokino           ###   ########.fr       */
+/*   Updated: 2022/03/20 02:17:00 by nlafarge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "2048.h"
+#include "file_scoring.h"
 
 void	ft_leaderboard(t_vars *vars)
 {
 	int	i;
-	char *leaderboard[] = {
-		" JEAN",
-		" LOUIS",
-		" MICHEL",
-		" CLAUDETTE",
-		" HUGO",
-		" LOUISE",
-		" JEANNETTE",
-		" JEAN-CLAUDE",
-		" BERNARD",
-		" LUC"
-	};
-	char *scores[] = {
-		"235345",
-		"435",
-		"346346",
-		"3",
-		"436",
-		"45",
-		"3452",
-		"34657679",
-		"232",
-		"8087"
-	};
-	int	leaderboard_size = sizeof(leaderboard) / sizeof(char *);
-	int leaderboard_height = leaderboard_size; // int = nb lines hard coded in the print of the menu
+
+	t_score *scores = NULL;
+	int res = get_scores_from_file(&scores);
 
 	char *menu[] = {
 		" BACK "
 	};
 	int menu_size = sizeof(menu) / sizeof(char *);
-	int menu_height = leaderboard_height + 5;
 
 	size_t max_length = 0;
 
@@ -57,7 +34,7 @@ void	ft_leaderboard(t_vars *vars)
 	{
 		i = 0;
 		ft_clear(vars); // My own clear function to also clear other variables (like cursor position)
-
+		
 		/*
 			MENU LOGIC
 		*/
@@ -74,26 +51,29 @@ void	ft_leaderboard(t_vars *vars)
 		*/
 		//ft_debug(vars);
 		ft_get_win_size(vars);
-		ft_vertical_align(vars, menu_height);
 		ft_print_center(vars, "2048");
 		ft_break_lines(vars, 2);
-		for (i = 0; i < leaderboard_size; i++)
+		t_score *tmpscores = scores;
+		while (tmpscores)
 		{
-			size_t tmp = ft_strlen(leaderboard[i]) + ft_strlen(scores[i]);
+			int tmp = ft_strlen(tmpscores->pseudo) + ft_strlen(ft_itoa(tmpscores->score));
 			if (tmp > max_length)
 				max_length = tmp;
+			tmpscores = tmpscores->next;
 		}
+
 		printw("\n\n");
-		for (i = 0; i < leaderboard_size; i++)
+		while(scores)
 		{
 			for (int x = 0; x < ((vars->nb_char_cols - max_length) / 2 - 3); x++)
 				printw(" ");
-			printw("%s ", leaderboard[i]);
-			for (size_t x = -1; x < (max_length - ft_strlen(leaderboard[i]) - ft_strlen(scores[i])); x++)
+			printw("%s ", scores->pseudo);
+			for (int x = -1; x < ((int)max_length - (int)ft_strlen(scores->pseudo) - (int)ft_strlen(ft_itoa(scores->score))); x++)
 				printw("-");
 			printw(" ");
-			printw("%s\n", scores[i]);
+			printw("%d\n", scores->score);
 			ft_break_line(vars);
+			scores = scores->next;
 		}
 		for (i = 0; i < menu_size; i++)
 		{
